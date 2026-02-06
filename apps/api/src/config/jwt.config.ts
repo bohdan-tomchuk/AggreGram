7862e5fb8d95c0d47530b93aa1413a29ce5e5ca6
Bohdan Tomchuk <bogdan.tomchuck@gmail.com>
2026-02-06 14:36:52 +0200

    wipe project

--- apps/api/src/config/jwt.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('jwt', () => ({
-  accessSecret: process.env.JWT_ACCESS_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  accessExpiresIn: '15m',
-  refreshExpiresIn: '7d',
-}));
